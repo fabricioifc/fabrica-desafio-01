e0ae12f470b171b84ac6dac69bcbc1ba436ce258
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-03-07 21:26:09 -0300

    json bug

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -38,30 +38,39 @@ class Content extends Component
   {
     const grup = []
     const apidb = GetApi()
-    console.log(apidb)
-    grup.push(
-      <>
-        {(apidb == null)?(
-          <>
-            null
-          </>
-          ):(
-            <>
-            {apidb?.map((title, i) => {
-            <a href='{apidb[i].id}' className="card m-3" style={{width: "18rem"}}>
-              <img src={'#'} className="card-img-top" alt='img'/>
-              <div className="card-body">
-                <h5  key={i} className="card-title">{title}</h5>
-                <p className="card-text">{}/10</p>
-              </div>
-            </a>
-            })}
-            </>
-          )
-        }
+
+    if (apidb.hasOwnProperty("results")) {
+      apidb.result.map((item, i)=>{
+        <>
+        <a href='{}' className="card m-3" style={{width: "18rem"}}>
+          <img src={'#'} className="card-img-top" alt='img'/>
+          <div className="card-body">
+            <h5 key={i} className="card-title">{item.title}</h5>
+            <p className="card-text">{}/10</p>
+          </div>
+        </a>
       </>
-    )
-    return grup
+      })
+      grup.push(
+        <>
+          <a href='{}' className="card m-3" style={{width: "18rem"}}>
+            <img src={'#'} className="card-img-top" alt='img'/>
+            <div className="card-body">
+              <h5 className="card-title">{apidb.result}</h5>
+              <p className="card-text">{}/10</p>
+            </div>
+          </a>
+        </>
+      )
+      return grup
+    }else
+    {
+      return (
+                <a href='#' className="card m-3" style={{width: "18rem"}}>
+                    loading
+                </a>
+        )
+    }
   }
 
   render()
@@ -87,8 +96,53 @@ class Footer extends Component
   }
 }
 
-export default App;
-
+/*
+adult
+: 
+false
+backdrop_path
+: 
+"/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg"
+genre_ids
+: 
+(3) [28, 12, 878]
+id
+: 
+693134
+media_type
+: 
+"movie"
+original_language
+: 
+"en"
+original_title
+: 
+"Dune: Part Two"
+overview
+: 
+"Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee."
+popularity
+: 
+1380.555
+poster_path
+: 
+"/8b8R8l88Qje9dn9OE8PY05Nxl1X.jpg"
+release_date
+: 
+"2024-02-27"
+title
+: 
+"Dune: Part Two"
+video
+: 
+false
+vote_average
+: 
+8.456
+vote_count
+: 
+985
+*/
 
 function GetApi() 
 {
@@ -97,7 +151,7 @@ function GetApi()
   useEffect( 
     ()=> {
       fetch("/api").then(
-        Response => Response.json()
+        response => response.json()
       ).then(
         data => {
           setBackendData(data)
@@ -106,3 +160,5 @@ function GetApi()
     }, [])
     return backendData
 }
+
+export default App;
