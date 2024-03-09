c9476f4ca8ceb8a37d585af94970a15fafc59f3e
Paulo Sérgio Pierdoná <paulosergiopierdona@gmail.com>
2024-03-09 20:34:40 -0300

    finished

--- client/myapp/src/App.js
+++ client/myapp/src/App.js
@@ -45,7 +45,7 @@ class Details extends Component{
     if (apidb.length >= 2) {
       let i = 0
       for(i = 0; i < apidb.length; i++){
-        if(("/"+apidb[i].id) !== id){
+        if(("/"+apidb[i].id) === id){
           break;
         }
       }
@@ -53,6 +53,7 @@ class Details extends Component{
       {
         let grup = []
         let element = apidb[i];
+        console.log(element)
         //name or title
         let title = element["title"] + ""
         if(title === 'undefined'){
@@ -60,12 +61,14 @@ class Details extends Component{
         }
         grup.push(
           <>
-            <div className="card m-3 link-underline link-underline-opacity-0 m-3" style={{width: "18rem", height: "18rem"}}>
-              <img src={`https://image.tmdb.org/t/p/w300/${element.backdrop_path}`} className="card-img-top" alt='img'/>
-              <div className="card-body d-flex flex-wrap justify-content-start" style={{height: "100%"}}>
-                <h5 className="fw-bolder card-title align-self-start col-12" title={title}>{title}</h5>
-                <p className="card-text align-self-end col-12">{element.popularity}⭐</p>
-              </div>
+            <div className="d-flex col-12 justify-content-center align-items-center p-2" >
+              <img src={`https://image.tmdb.org/t/p/w300/${element.poster_path}`} className="col-4" alt='img'/>
+            </div>
+            <div className="d-flex flex-wrap justify-content-start col-12 p-3 border-top-2 border-top" style={{height: "100%"}}>
+              <h5 className="fw-bolder col-12 p-2" title={title}>{title}</h5>
+              <p className="col-12 p-2">lançamento: {element.first_air_date}</p>
+              <p className="col-12 p-2">{element.popularity}⭐</p>
+              <p className="col-12 p-2">Visão geral:{element.overview}</p>
             </div>
           </>
         )
@@ -91,7 +94,13 @@ class Details extends Component{
   render()
   {
     return (
-      <this.getItem/>
+      <main className="d-flex flex-wrap justify-content-center align-items-start container-fluid col-12 p-0 mt-5 mb-5" style={{minHeight: "100vh"}}>
+      <div className='d-flex flex-wrap justify-content-center align-items-center container-fluid col-10 border border-2' style={{height: "100%"}}>
+        <>
+          <this.getItem/>
+        </>
+      </div>
+    </main>
     )
   }
 }
